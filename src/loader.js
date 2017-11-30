1bc519af7c6df07eba593ea9a3eb178b3325b088
Jonathan Smith <jonathan@peoplesized.com>
2017-11-30 20:22:36 +0200

    more

--- src/loader.js
+++ src/loader.js
@@ -4,9 +4,14 @@ import compareVersions from 'compare-versions'
 (async () => {
   const match_semver_cache = /\bv?(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)(?:-[\da-z-]+(?:\.[\da-z-]+)*)?(?:\+[\da-z-]+(?:\.[\da-z-]+)*)?\b/ig
   const body = document.querySelector('body')
-  
-  const fetch_version = await fetch('/latest/VERSION')
-  const latest_version = (await fetch_version.text()).replace('v', '')
+
+  let latest_version
+  try {
+    const fetch_version = await fetch('/latest/VERSION')
+    latest_version = (await fetch_version.text()).replace('v', '')
+  } catch(e) {
+    console.log('Fetch for latest version failed - offline?')
+  }
 
   const keys = await caches.keys()
 
