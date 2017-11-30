9baee9cc02fc37086806b1b8c7115cc2cfda43d6
Jonathan Smith <jonathan@peoplesized.com>
2017-11-30 20:26:40 +0200

    more

--- app-sw-precache-config.js
+++ app-sw-precache-config.js
@@ -4,8 +4,6 @@ module.exports = {
   maximumFileSizeToCacheInBytes: 14194304,
   navigateFallback: 'index.html',
   staticFileGlobsIgnorePatterns: [
-    /.js/,
-    /.html/,
     /\.map$/,
     /VERSION/,
     /COMMITHASH/
