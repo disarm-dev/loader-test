1af007af52da57acc6804dc70c1cb13056011349
Jonathan Smith <jonathan@peoplesized.com>
2017-12-01 13:34:59 +0200

    more

--- webpack.config.js
+++ webpack.config.js
@@ -33,71 +33,71 @@ module.exports = [
       ]),
     ]
   },
-  // {
-  //   entry: './src/main.js',
-  //   output: {
-  //     path: path.resolve(__dirname, './dist', version),
-  //     publicPath: `/${version}/`,
-  //     filename: '[name].js'
-  //   },
-  //   module: {
-  //     rules: [
-  //       {
-  //         test: /\.css$/,
-  //         use: [
-  //           'vue-style-loader',
-  //           'css-loader'
-  //         ],
-  //       }, {
-  //         test: /\.vue$/,
-  //         loader: 'vue-loader',
-  //         options: {
-  //           loaders: {}
-  //           // other vue-loader options go here
-  //         }
-  //       },
-  //       // {
-  //       //   test: /\.js$/,
-  //       //   loader: 'babel-loader',
-  //       //   exclude: /node_modules/
-  //       // },
-  //       {
-  //         test: /\.(png|jpg|gif|svg)$/,
-  //         loader: 'file-loader',
-  //         options: {
-  //           name: '[name].[ext]?[hash]'
-  //         }
-  //       }
-  //     ]
-  //   },
-  //   plugins: [
-  //     new webpack.DefinePlugin({
-  //       __VERSION_COMMIT_HASH_SHORT: JSON.stringify(version)
-  //     }),
-  //     new CopyWebpackPlugin([
-  //       {from: './app.html', to: './index.html'},
-  //     ]),
-  //     new SWPrecacheWebpackPlugin(require('./app-sw-precache-config.js')),
-  //     gitRevisionPlugin, // Write VERSION and COMMITHASH files
-  //     // new Visualizer(),
-  //     new WebpackShellPlugin({onBuildEnd:[`ln -sfn ${version} ./dist/latest`]})
-  //   ],
-  //   resolve: {
-  //     alias: {
-  //       'vue$': 'vue/dist/vue.runtime.esm.js'
-  //     },
-  //     extensions: ['*', '.js', '.vue', '.json']
-  //   },
-  //   devServer: {
-  //     historyApiFallback: true,
-  //     noInfo: true,
-  //     overlay: true
-  //   },
-  //   performance: {
-  //     hints: false
-  //   },
-  //   devtool: '#eval-source-map'
-  // }
+  {
+    entry: './src/main.js',
+    output: {
+      path: path.resolve(__dirname, './dist', version),
+      publicPath: `/${version}/`,
+      filename: '[name].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            'vue-style-loader',
+            'css-loader'
+          ],
+        }, {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+          options: {
+            loaders: {}
+            // other vue-loader options go here
+          }
+        },
+        // {
+        //   test: /\.js$/,
+        //   loader: 'babel-loader',
+        //   exclude: /node_modules/
+        // },
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]?[hash]'
+          }
+        }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        __VERSION_COMMIT_HASH_SHORT: JSON.stringify(version)
+      }),
+      new CopyWebpackPlugin([
+        {from: './app.html', to: './index.html'},
+      ]),
+      new SWPrecacheWebpackPlugin(require('./app-sw-precache-config.js')),
+      gitRevisionPlugin, // Write VERSION and COMMITHASH files
+      // new Visualizer(),
+      new WebpackShellPlugin({onBuildEnd:[`ln -sfn ${version} ./dist/latest`]})
+    ],
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.runtime.esm.js'
+      },
+      extensions: ['*', '.js', '.vue', '.json']
+    },
+    devServer: {
+      historyApiFallback: true,
+      noInfo: true,
+      overlay: true
+    },
+    performance: {
+      hints: false
+    },
+    devtool: '#eval-source-map'
+  }
 ]
 
 if (process.env.NODE_ENV === 'production') {
